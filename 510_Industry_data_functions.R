4fbb9763ab75f7217b5f0ac4d51172794d9be56c
DagHjermann <>
2021-02-16 12:46:02 +0000

    plot_medians_and_trends2 improved
    Made independent of 'big excel file' (dfbig)
    Instead
    - runs model_from_medians on data
    - gets EQS and PROREF from separate files

--- 510_Industry_data_functions.R
+++ 510_Industry_data_functions.R
@@ -1,4 +1,10 @@
 
+#
+# read_last_file
+#
+# Reads the last file with a certain name pattern
+# Meant for the MILKLYS data flow  
+#
 
 read_last_file <- function(folder = "Data", 
                            pattern = NULL,
@@ -198,56 +204,90 @@ get_proref <- function(folder = "Input_data"){
 #
 # OVERVIEW:
 #   calls get_plotdata() 
-#      - gets data from data_med2
+#      - gets data from data_median
+#        (must contain 'PARAM', 'LATIN_NAME', 'TISSUE_NAME', 'Basis', 'STATION_CODE', 'MYEAR', 'Median')
 #      - runs trend analysis using model_from_medians()
 #      - returns median data + trend lines as a big list object (X)
-#   calls get_dfbig()
-#      - gets the relevant line from the big excel file
-#      - gets EQS and trend symbols from here
-#      - returns dfbig = one line of the big excel file
+#   gets Proref from a separate file 
+#     (must contain 'PARAM', 'LATIN_NAME', 'TISSUE_NAME', 'Basis' and 'Limit')
+#   gets EQS from a separate file 
+#     (must contain 'PARAM' and 'Limit')
 #   calls plot_medians_color() with X (for points + trend), proref and EQS (from dfbig)
 #      - makes plot, except trend symbols
 #      - returns plot (gg) 
-#   creates trends (= arrowup, circle etc.)
+#   calls set_symbol_word() to get trends (= 'arrowup', 'circle' etc.)
 #   calls add_trend() with gg + trends as input
 #      - adds plot symbols to the plot (gg)
 #   create file name
 #   returns plot (gg) and file name (fn) 
 
 
-plot_medians_and_trends <- function(ser, x_rel = 0.8, y_rel = 0.9, xlim = NULL, ylim = NULL, 
-                                    titlesize = 0.85, trend_years = NULL, ...){
+
+
+plot_medians_and_trends2 <- function(ser, x_rel = 0.8, y_rel = 0.9, xlim = NULL, ylim = NULL, 
+                                    titlesize = 0.85, 
+                                    trend_years = NULL, 
+                                    data_medians,
+                                    data_proref, 
+                                    data_eqs, 
+                                    trendsymbols_x_spacing = c(0, 0.030, 0.065),
+                                    windows = FALSE,
+                                    ...){
   
   if (is.null(trend_years) & !is.null(xlim))
     trend_years <- seq(xlim[1], xlim[2])
   if (is.null(trend_years) & is.null(xlim))
     stop("You must supply either xlim or trend_years")
   X <- get_plotdata(ser[1], ser[2], ser[3], ser[4], ser[5],
-                    trend_years = trend_years)
+                    trend_years = trend_years, 
+                    data_medians = data_medians)
+  # Used to set x_axis limits:
   start_yr <-  2*floor(min(X$df_data$MYEAR)/2)
+  end_yr <-  2*ceiling(max(X$df_data$MYEAR)/2)
+  
+  time_trend_long <- model_from_medians(ser[1], ser[2], ser[3], ser[4], ser[5],
+                                        trend_years, 
+                                   data_medians = data_medians, 
+                                   ggplot = FALSE)$statistics
+  time_trend_10yr <- model_from_medians(ser[1], ser[2], ser[3], ser[4], ser[5],
+                                        tail(trend_years, 11),  # 11 because first + last years are included 
+                                        data_medians = data_medians, 
+                                        ggplot = FALSE)$statistics
   
   # The next is needed because the big excel file for 2018 (made in 2019) uses
   #   English tissue names
-  dfbig_tissue <- case_when(
-    ser[3] == "Lever" ~ "Liver",
-    ser[3] == "Muskel" ~ "Muscle",
-    TRUE ~ ser[3])
-  dfbig <- get_dfbig(ser[1], ser[2], dfbig_tissue, ser[4], ser[5])
-  
-  # Last year
-  last_year <- grep("Yr", names(dfbig), value = TRUE) %>% sub("Yr_", "", .)  %>% as.numeric() %>% max()
+  if (FALSE){
+    dfbig_tissue <- case_when(
+      ser[3] == "Lever" ~ "Liver",
+      ser[3] == "Muskel" ~ "Muscle",
+      TRUE ~ ser[3])
+    dfbig <- get_dfbig(ser[1], ser[2], dfbig_tissue, ser[4], ser[5])
+  }
   
   # Proref from median data
   # proref <- X$df_data$Q95[1]
   
-  # Proref from big excel data
-  proref <- dfbig$Q95[1]
+  # Proref from a separate file (must contain 'PARAM', 'LATIN_NAME', 'TISSUE_NAME', 'Basis' and 'Limit')
+  proref <- data_proref %>%
+    filter(PARAM == ser[1] & LATIN_NAME == ser[2] & TISSUE_NAME == ser[3] & Basis == ser[5]) %>% # 4 = station
+    pull(Q95)
+  if (length(proref) == 0)
+    proref <- NA
   
-  cat("PROREF = ", proref, ", EQS = ", dfbig$EQS, "\n")
+  # EQS from a separate file (must contain 'PARAM' and 'Limit')
+  eqs <- data_eqs %>%
+    filter(PARAM == ser[1]) %>% 
+    pull(Limit)
+
+  # Print to screen
+  cat("PROREF = ", proref)
+  if (length(eqs) > 0)
+    cat(", EQS = ", eqs)
+  cat("\n")
   
   # Axis limits - note some hard-coding if xlim = NULL
   if (is.null(xlim))
-    xlim <- c(start_yr,2020) - 2  # to make room for PROREF text
+    xlim <- c(start_yr, end_yr) - 2  # to make room for PROREF text
   if (is.null(ylim))
     ylim <- c(0, max(X$df_data$Median)*1.15)
   
@@ -281,8 +321,7 @@ plot_medians_and_trends <- function(ser, x_rel = 0.8, y_rel = 0.9, xlim = NULL, 
     yrstep)
   
   # Start plot.   ELU: testing "geom_text(aes(x=Inf, y=Inf, hjust=10.0, vjust=2.7, label = "\u25AA"), size = rel(8))"
-  
-  gg <- plot_medians_color(X, proref, xlim = xlim, eqs = dfbig$EQS, ylim = ylim, ...) +
+  gg <- plot_medians_color(X, proref, xlim = xlim, eqs = eqs[1], ylim = ylim, ...) +
     scale_x_continuous(minor_breaks = seq(xlim[1], xlim[2]), breaks = breaks) +
     ggtitle(plot_title) +
     
@@ -292,18 +331,12 @@ plot_medians_and_trends <- function(ser, x_rel = 0.8, y_rel = 0.9, xlim = NULL, 
     
     theme(title = element_text(size = rel(titlesize)))
   
-  # Add trend symbols. 
-  trend_variable <- paste0("Trends.", last_year)
-  trendsymbol_xl <- c(substr(dfbig[[trend_variable]], 1, 1), substr(dfbig[[trend_variable]], 3, 3))
-  trends <- case_when(
-    trendsymbol_xl %in% "¢" ~ "circle",
-    trendsymbol_xl %in% "é" ~ "arrowup",
-    trendsymbol_xl %in% "ê" ~ "arrowdown",
-    trendsymbol_xl %in% "§" ~ "filledsquare",
-  )
-  
+  # Add trend symbol
+  trends <- set_symbol_word(bind_rows(time_trend_long, time_trend_10yr))
   gg <- add_trend(gg, trendsymbols = trends, x_rel = x_rel, y_rel = y_rel, 
-                  x_spacing = c(0, 0.035, 0.065), fontsize = 10)
+                  x_spacing = trendsymbols_x_spacing, 
+                  fontsize = 10,
+                  windows = windows)
   
   
   # Make file name (but does not save file)
