761eb7bec3b653bcb7b984ef303ed20a06aa259e
DagHjermann <>
2022-09-30 04:17:27 +0000

    Add button for saving plot + unit on y axis
    - Also add proper y axis with unit

--- 402_Plot_time_series_functions.R
+++ 402_Plot_time_series_functions.R
@@ -78,6 +78,9 @@ plot_timeseries_seriesno <- function(seriesno,
   df_points <- extract_raw_data(seriesno, data = data, data_series = data_series) %>%
     mutate(y_comb = ifelse(is.na(y), threshold, y))
   
+  # Get unit to print on y axis  
+  unit_print <- get_unit_text(tail(df_points$UNIT, 1), "WW", tail(df_points$PARAM, 1))
+  
   df_median <- df_points %>%
     group_by(x) %>%
     summarise(
@@ -144,11 +147,80 @@ plot_timeseries_seriesno <- function(seriesno,
         scale_y_log10()
     }
     gg <- gg +
+      labs(y = unit_print, x = "") +
       annotate("text", x = Inf, y = Inf, label = trendstring, hjust = 1.1, vjust = 1.2, size = trendtext_size, colour = "blue3")
   }
   
   gg
   
+}
+
+#
+# Given unit, basis and param, make a suitable "unit text" for y axis label
+#
+get_unit_text <- function(unit, basis, param){
+  # Unit
+  if (param %in% "VDSI"){                                  # special treatment of VDSI
+    unit_print <- "VDSI"                                    #endret fra "VDS average"
+  } else if (basis %in% "WW" & unit %in% "MG_P_KG"){       # if it is something else than VDSI, we check basis and unit
+    unit_print <- "mg/kg (w.w.)"
+  } else if (basis %in% "WW" & unit %in% "UG_P_KG"){
+    unit_print <- expression(mu*g/kg~(w.w.))
+  } else if (basis %in% "DW" & unit %in% "MG_P_KG"){
+    unit_print <- "mg/kg (d.w.)"
+  } else if (basis %in% "DW" & unit %in% "UG_P_KG"){
+    unit_print <- expression(mu*g/kg~(d.w.))
+  } else if (basis %in% "FB" & unit %in% "MG_P_KG"){
+    unit_print <- "mg/kg (lipid basis)"
+  } else if (basis %in% "FB" & unit %in% "UG_P_KG"){
+    unit_print <- expression(mu*g/kg~(lipid~basis))
+  } else if (basis %in% "WWa" & unit %in% "MG_P_KG"){
+    unit_print <- "mg/kg (w.w.) for 50 cm cod"
+  } else if (basis %in% "WWa" & unit %in% "UG_P_KG"){
+    unit_print <- expression(mu*g/kg~(w.w.)~plain("for 50 cm cod"))
+  } else if (basis %in% "DWa" & unit %in% "MG_P_KG"){
+    unit_print <- "mg/kg (d.w.) for 50 cm cod"
+  } else if (basis %in% "DWa" & unit %in% "UG_P_KG"){
+    unit_print <- expression(mu*g/kg~(d.w.)~plain("for 50 cm cod"))
+  } else if (basis %in% "FBa" & unit %in% "MG_P_KG"){
+    unit_print <- "mg/kg (lipid basis) for 50 cm cod"
+  } else if (basis %in% "FBa" & unit %in% "UG_P_KG"){
+    unit_print <- expression(mu*g/kg~(lipid~basis)~plain("for 50 cm cod"))
+  } else {
+    unit_print <- unit
+  }
+  unit_print
+}
+
+
+save_plot <- function(ggplot, fn, folder, suffix = "", windows = FALSE,
+                           width = 5.6, height = 4.4,                                  # set plot size, in inches (but see note in code for Linux)
+                           dpi_windows = 450, dpi_linux = 400                          # set plot resolution (dots per inch))
+){
+  # fn <- paste0(folder, "/", plot_single_result$fn)
+  # If you want, you can add a suffix to the file name (e.g. "_2") by 
+  #   setting e.g. suffix = "_2"  (note: works only on png and jpg!)
+  fn <- sub(".png", paste0(suffix, ".png"), fn)
+  fn <- sub(".jpg", paste0(suffix, ".jpg"), fn)
+  if (windows){
+    # If we are in Windows, we use ggsave() 
+    ggsave(fn, plot_single_result$gg, height = height, width = width, dpi = dpi_windows)
+  } else {
+    # If we are in Linux (e.g. Jupyterhub), we use 'good old' png()   
+    # Note that we also have to increase the size of the plot a bit (in inches),
+    #   otherwise the trend symbols look bad
+    # Because of that size increase, we also have to increase the size of 
+    #   numbers and labels first
+    ggplot <- ggplot +
+      theme(axis.text = element_text(size = 11),     # set size of numbers along axes
+            axis.title = element_text(size = 12),    # set size of axis labels
+            plot.title = element_text(size = 13))    # set size of plot title
+    #svglite(fn, height = height*1.2, width = width*1.2)                              ELU:testing with svglite (not png)
+    png(fn, height = height*1.2, width = width*1.2, units = "in", res = dpi_linux)    # create an empty plot file 
+    print(ggplot)                                                                         # ...plot on it...
+    dev.off()                                                                         # ...and save it
+  }
+  invisible(fn)   # returns file name invisibly
 }
 
 
