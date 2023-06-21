58f9a8034770bb1d98acce9af6ab0bd2fe710352
DagHjermann <>
2023-06-21 13:54:34 +0000

    App 04, first static version

--- App04_sum_parameters/app.R
+++ App04_sum_parameters/app.R
@@ -7,7 +7,61 @@
 #    http://shiny.rstudio.com/
 #
 
+# For testing code parts:
+# setwd("App04_sum_parameters")
+# After testing:
+# setwd(".."); getwd()
+
 library(shiny)
+
+library(dplyr)
+library(ggplot2)
+library(glue)
+
+source("../002_Utility_functions.R")
+source("../101_Combine_with_legacy_data_functions.R")
+
+data_all2 <- readRDS("../Data/109_adjusted_data_2022-09-23.rds")  
+
+param <- "BDE6S"
+
+data_all2_by_samplepar <- data_all2 %>%
+  filter(PARAM %in% sum_parameters[[param]],
+         MYEAR >= 2010) %>% # View()  
+  mutate(
+    VALUE_WW_wo_loq = case_when(
+      is.na(FLAG1) ~ VALUE_WW,
+      !is.na(FLAG1) ~ 0)
+  )
+
+data_all2_by_sample <- data_all2_by_samplepar %>%
+  group_by(STATION_CODE, LATIN_NAME, TISSUE_NAME, MYEAR, SAMPLE_NO2, UNIT) %>%
+  summarize(
+    VALUE_lb = sum(VALUE_WW_wo_loq),
+    VALUE_ub = sum(VALUE_WW),
+    n_param = n(), .groups = "drop")
+
+data_all2_by_station <- data_all2_by_sample %>%
+  group_by(STATION_CODE, LATIN_NAME, TISSUE_NAME, MYEAR, UNIT) %>%
+  summarize(
+    N_over_zero = sum(VALUE_lb > 0),
+    Min_over_zero = min(VALUE_lb[VALUE_lb > 0]),
+    Max_over_zero = max(VALUE_lb[VALUE_lb > 0]),
+    VALUE_lb = median(VALUE_lb),
+    VALUE_ub = median(VALUE_ub), .groups = "drop") %>%
+  mutate(
+    DDI = case_when(
+      N_over_zero > 1 ~ glue("{N_over_zero} [{round(Min_over_zero,2)}-{round(Max_over_zero,2)}]"),
+      N_over_zero == 1 ~ glue("{N_over_zero} [{round(Max_over_zero,2)}]"),
+      N_over_zero == 0 ~ glue("{N_over_zero} (-)")
+    )
+  )
+
+data_plot <- data_all2_by_station %>%
+  filter(STATION_CODE == "I714")
+
+
+
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -44,6 +98,11 @@ server <- function(input, output) {
         hist(x, breaks = bins, col = 'darkgray', border = 'white',
              xlab = 'Waiting time to next eruption (in mins)',
              main = 'Histogram of waiting times')
+        
+        gg <- ggplot(data_plot, aes(MYEAR, ymin = VALUE_lb, ymax = VALUE_ub)) +
+          geom_errorbar(width = 0.2)
+        print(gg)
+        
     })
 }
 
