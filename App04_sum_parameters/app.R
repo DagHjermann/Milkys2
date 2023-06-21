830f7d08e655e2387c30b08a12b5220776dcdea2
DagHjermann <>
2023-06-21 14:09:13 +0000

    App 04, wortking dynamic version
    - stations menu still not dynamic

--- App04_sum_parameters/app.R
+++ App04_sum_parameters/app.R
@@ -27,44 +27,18 @@ params <- names(sum_parameters)
 
 param <- "BDE6S"
 
-data_all2_by_samplepar <- data_all2 %>%
-  filter(PARAM %in% sum_parameters[[param]],
-         MYEAR >= 2010) %>% # View()  
+data_all2_by_samplepar_all <- data_all2 %>%
+  filter(MYEAR >= 2010) %>% # View()  
   mutate(
     VALUE_WW_wo_loq = case_when(
       is.na(FLAG1) ~ VALUE_WW,
       !is.na(FLAG1) ~ 0)
   )
 
-data_all2_by_sample <- data_all2_by_samplepar %>%
-  group_by(STATION_CODE, LATIN_NAME, TISSUE_NAME, MYEAR, SAMPLE_NO2, UNIT) %>%
-  summarize(
-    VALUE_lb = sum(VALUE_WW_wo_loq),
-    VALUE_ub = sum(VALUE_WW),
-    n_param = n(), .groups = "drop")
-
-data_all2_by_station <- data_all2_by_sample %>%
-  group_by(STATION_CODE, LATIN_NAME, TISSUE_NAME, MYEAR, UNIT) %>%
-  summarize(
-    N_over_zero = sum(VALUE_lb > 0),
-    Min_over_zero = min(VALUE_lb[VALUE_lb > 0]),
-    Max_over_zero = max(VALUE_lb[VALUE_lb > 0]),
-    VALUE_lb = median(VALUE_lb),
-    VALUE_ub = median(VALUE_ub), .groups = "drop") %>%
-  mutate(
-    DDI = case_when(
-      N_over_zero > 1 ~ glue("{N_over_zero} [{round(Min_over_zero,2)}-{round(Max_over_zero,2)}]"),
-      N_over_zero == 1 ~ glue("{N_over_zero} [{round(Max_over_zero,2)}]"),
-      N_over_zero == 0 ~ glue("{N_over_zero} (-)")
-    )
-  )
-
-stations <- unique(data_all2_by_station$STATION_CODE)
+stations <- unique(data_all2_by_samplepar_all$STATION_CODE)
 
 data_plot <- data_all2_by_station %>%
   filter(STATION_CODE == "I714")
-
-
 
 
 # Define UI for application that draws a histogram
@@ -90,13 +64,48 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
   
-    output$distPlot <- renderPlot({
-      
-        gg <- ggplot(data_plot, aes(MYEAR, ymin = VALUE_lb, ymax = VALUE_ub)) +
-          geom_errorbar(width = 0.2)
-        print(gg)
-        
-    })
+  
+  data_parameter <- reactive({
+    data_all2_by_sample <- data_all2_by_samplepar_all %>%
+      filter(PARAM %in% sum_parameters[[input$param]]) %>%
+      group_by(STATION_CODE, LATIN_NAME, TISSUE_NAME, MYEAR, SAMPLE_NO2, UNIT) %>%
+      summarize(
+        VALUE_lb = sum(VALUE_WW_wo_loq),
+        VALUE_ub = sum(VALUE_WW),
+        n_param = n(), .groups = "drop")
+    
+    data_all2_by_station <- data_all2_by_sample %>%
+      group_by(STATION_CODE, LATIN_NAME, TISSUE_NAME, MYEAR, UNIT) %>%
+      summarize(
+        N_over_zero = sum(VALUE_lb > 0),
+        Min_over_zero = min(VALUE_lb[VALUE_lb > 0]),
+        Max_over_zero = max(VALUE_lb[VALUE_lb > 0]),
+        VALUE_lb = median(VALUE_lb),
+        VALUE_ub = median(VALUE_ub), .groups = "drop") %>%
+      mutate(
+        DDI = case_when(
+          N_over_zero > 1 ~ glue("{N_over_zero} [{round(Min_over_zero,2)}-{round(Max_over_zero,2)}]"),
+          N_over_zero == 1 ~ glue("{N_over_zero} [{round(Max_over_zero,2)}]"),
+          N_over_zero == 0 ~ glue("{N_over_zero} (-)")
+        )
+      )
+    data_all2_by_station
+  })
+  
+  data_parameter_station <- reactive({
+    data_parameter() %>%
+      filter(STATION_CODE %in% input$station)
+  })
+    
+    
+  output$distPlot <- renderPlot({
+    
+    data_plot <- data_parameter_station()
+    gg <- ggplot(data_plot, aes(MYEAR, ymin = VALUE_lb, ymax = VALUE_ub)) +
+      geom_errorbar(width = 0.2)
+    print(gg)
+    
+  })
 }
 
 # Run the application 
