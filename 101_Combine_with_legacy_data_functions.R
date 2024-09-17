d20f44ff7218c99511280f451428eeb5c2a9b062
DagHjermann <>
2024-09-17 20:13:38 +0000

    101 func: add_sumparameter_exloq_allyears copied

--- 101_Combine_with_legacy_data_functions.R
+++ 101_Combine_with_legacy_data_functions.R
@@ -231,6 +231,85 @@ add_sumparameter_exloq_allyears <- function(i, pars_list, data){
 }
 
 
+
+# In contrast to add_sumparameter (which is only for last year's data), this is for use on entire data series (dat_updated2) 
+# - for all columns VALUE_WW, VALUE_DW, VALUE_FB, VALUE_WWa, VALUE_DWa, VALUE_FBa
+# - extra columns DRYWT, FAT_PERC, LNMEA, STATION_NAME, SAMPLE_DATE (we drop UNCERTAINTY, QUANTIFICATION_LIMIT etc.)
+#
+# NOTE!
+# - used only in 2022 (when 2021 data were ingested), in order to update last year's data. Does usually not need to be run!
+
+add_sumparameter_inclloq_allyears <- function(i, pars_list, data){
+  # Add variable N_par, if it's not already there
+  if (!"N_par" %in% colnames(data)){
+    data$N_par <- 1
+  }
+  pars <- pars_list[[i]]
+  cat("==================================================================\n", i, names(pars_list)[i], "\n")
+  cat(pars, "\n")
+  # Reshape data (from VALUE_WW, VALUE_DW, etc.  to VALUE and BASIS columns)
+  df_reshaped <- data %>%
+    filter(PARAM %in% pars & !is.na(SAMPLE_NO2)) %>%                        # select records (only those with SAMPLE_NO2)
+    select(
+      STATION_CODE, LATIN_NAME, TISSUE_NAME, MYEAR, SAMPLE_NO2, UNIT, 
+      FLAG1, VALUE_WW, VALUE_DW, VALUE_FB, VALUE_WWa, VALUE_DWa, VALUE_FBa) %>%
+    tidyr::pivot_longer(
+      cols = c(VALUE_WW, VALUE_DW, VALUE_FB, VALUE_WWa, VALUE_DWa, VALUE_FBa), names_to = "BASIS", values_to = "VALUE") %>%
+    mutate(
+      VALUE_exloq = case_when(
+        is.na(FLAG1) ~ VALUE,
+        !is.na(FLAG1) ~ 0)
+    )
+  df_grouped <- df_reshaped %>%
+    group_by(STATION_CODE, LATIN_NAME, TISSUE_NAME, MYEAR, SAMPLE_NO2, UNIT, BASIS) # not PARAM, but including BASIS
+  if (nrow(df_grouped) > 0){
+    df1 <- df_grouped %>%
+      summarise(VALUE = sum(VALUE_exloq, na.rm = TRUE), .groups = "drop_last") %>%      # sum of the measurements
+      mutate(QUANTIFICATION_LIMIT = NA) %>%
+      as.data.frame(stringsAsFactors = FALSE)
+    df3 <- df_grouped %>%
+      summarise(N_par = n(), .groups = "drop_last") %>%    # number of measurements
+      as.data.frame()
+    # Should be all 1
+    # check <- df1[,1:9] == df2[,1:9]
+    # cat("Test 1 (should be 1):", 
+    #     apply(check, 2, mean) %>% mean(na.rm = TRUE), "\n")
+    # 
+    # check <- df2[,1:9] == df3[,1:9]
+    # cat("Test 2 (should be 1):", 
+    #     apply(check, 2, mean) %>% mean(na.rm = TRUE), "\n")
+    
+    # Set the parameter name
+    df1$PARAM <- paste0(names(pars_list)[i], "_exloq")   
+    
+    df_to_add1 <- data.frame(df1, FLAG1 = as.character(NA), N_par = df3[,"N_par"], stringsAsFactors = FALSE)  # Make data to add
+    
+    # Get and add support parameters
+    df_supportpars <- data %>%
+      filter(PARAM %in% pars & !is.na(SAMPLE_NO2)) %>%  
+      group_by(STATION_CODE, LATIN_NAME, TISSUE_NAME, MYEAR, SAMPLE_NO2, UNIT) %>%
+      summarise(
+        DRYWT = mean(DRYWT, na.rm = TRUE),
+        FAT_PERC = mean(FAT_PERC, na.rm = TRUE),
+        LNMEA = mean(LNMEA, na.rm = TRUE),
+        STATION_NAME = first(STATION_NAME),
+        SAMPLE_DATE = first(SAMPLE_DATE), .groups = "drop")
+    df_to_add2 <- df_to_add1 %>%
+      left_join(df_supportpars, by = c("STATION_CODE", "LATIN_NAME", "TISSUE_NAME", "MYEAR", "SAMPLE_NO2", "UNIT"))
+    
+    # Reshape data (get back columns VALUE_WW, VALUE_DW, etc.  )
+    df_to_add_reshaped <- df_to_add2 %>%
+      tidyr::pivot_wider(names_from = BASIS, values_from = VALUE)
+    data <- bind_rows(data, df_to_add_reshaped)   # Add data for this parameter
+    cat("Number of rows added:", nrow(df_to_add_reshaped), "; number of rows in data:", nrow(data), "\n")
+  } else {
+    cat("No rows added (found no data for these parameters)\n")
+  }
+  data
+}
+
+
+
 # Summarises a sequence into a string, e.g. "1991-1993,1996-1997,2000"
 summarize_sequence <- function(x){
   x <- sort(unique(x))
