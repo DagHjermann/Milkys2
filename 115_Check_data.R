e2b62b7c4b658d3752dec9373d37ecb5de14cf1a
DagHjermann <>
2024-10-01 14:11:20 +0000

    scr 115: add EROD and ALAD stuff

--- 115_Check_data.R
+++ 115_Check_data.R
@@ -2,6 +2,10 @@
 library(ggplot2)
 library(dplyr)
 
+#
+# read data -----------------------------------------------------------
+#
+
 # Check last year's data, downloaded from Nivabase
 dat_nivabase <- readRDS("Input_data/80_df_2023_notstandard_2024-09-23_20h06m.rds")
 
@@ -12,18 +16,33 @@ dat_103 <- readRDS("Data/103_data_updated_2024-09-25.rds")
 dat_105 <- readRDS("Data/105_data_with_uncertainty_2024-09-25.rds")
 dat_109 <- readRDS("Data/109_adjusted_data_ELU_2024-09-25.rds")
 
+#
+# FLAG1 -----------------------------------------------------------
+#
+
 # table: FLAG1 (under LOQ) + MYEAR
 xtabs(~addNA(FLAG1) + MYEAR, subset(dat_nivabase, MYEAR > 2018))
 xtabs(~addNA(FLAG1) + MYEAR, subset(dat_101, MYEAR > 2018))
 xtabs(~addNA(FLAG1) + MYEAR, subset(dat_109, MYEAR > 2018))
 
+#
+# stations -----------------------------------------------------------
+#
 # table: STATION_CODE + MYEAR
 xtabs(~STATION_CODE + MYEAR, subset(dat_nivabase, MYEAR > 2018))
 xtabs(~STATION_CODE + MYEAR, subset(dat_109, MYEAR > 2018))
 
+#
+# eider duck -----------------------------------------------------------
+#
+
 # table: PARAM + MYEAR for eider duck  
 xtabs(~NAME + MYEAR, subset(dat_nivabase, MYEAR > 2018 & STATION_CODE == "19N"))
 xtabs(~PARAM + MYEAR, subset(dat_109, MYEAR > 2018 & STATION_CODE == "19N"))
+
+#
+# xCCP -----------------------------------------------------------
+#
 
 # SCCP and MCCP - check whether values are missing (NA), zero or >zero 
 dat_109 %>%
@@ -38,7 +57,36 @@ dat_109 %>%
   ) %>%
   xtabs(~VALUE_class + MYEAR, .)
 
-# test plot some substances, eider duck 
+#
+# EROD og ALAD -----------------------------------------------------------
+#
+# SCCP and MCCP - check whether values are missing (NA), zero or >zero 
+dat_nivabase %>%
+  filter(
+    NAME %in% c("EROD", "ALAD", "PROTV") & !is.na(VALUE) & MYEAR >= 2010,
+  ) %>%
+  xtabs(~STATION_CODE + MYEAR + NAME + METHOD_ID, .)
+dat_nivabase %>%
+  filter(
+    NAME %in% c("EROD", "ALAD", "PROTV") & !is.na(VALUE) & MYEAR >= 2010,
+  ) %>% pull(SAMPLE_ID) %>% unique() %>% sort()
+dat_109 %>%
+  filter(
+    PARAM %in% c("EROD", "ALAD") & !is.na(VALUE_WW) & MYEAR >= 2010,
+  ) %>%
+  xtabs(~STATION_CODE + MYEAR + PARAM, .)
+
+check <- readRDS("Input_data/01_df_2022_notstandard_2023-09-12.rds")
+check %>%
+  filter(
+    NAME %in% c("EROD", "ALAD") & !is.na(VALUE),
+  ) %>%
+  xtabs(~STATION_CODE + NAME, .)
+
+
+#
+# test plot some substances, eider duck ------------------------------------
+#
 substance <- "D5"
 substance <- "HG"
 substance <- "PFOS"
